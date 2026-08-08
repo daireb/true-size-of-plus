31f0078065d16853c4afccd9b9b6bf829a9b03ac
daireb <bohan.daire@gmail.com>
2026-08-08 19:02:54 +0100

    Derive the Pages push URL from the origin remote

--- scripts/deploy-pages.sh
+++ scripts/deploy-pages.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # Build for GitHub Pages and force-push the output to the gh-pages branch.
 set -e
+REMOTE=$(git remote get-url origin)
 GHPAGES=1 npm run build
 cd dist
 rm -rf .git
@@ -8,6 +9,6 @@ git init -q
 git checkout -qb gh-pages
 git add -A
 git -c user.name="daireb" -c user.email="bohan.daire@gmail.com" commit -qm "deploy"
-git push -f https://github.com/daireb/true-size-of-plus.git gh-pages
+git push -f "$REMOTE" gh-pages
 rm -rf .git
 echo "deployed."
